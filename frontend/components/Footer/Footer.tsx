19fd1e36de4549a6e1aac643168e1e601848184e
antoniohauren <antoniohauren@protonmail.com>
2022-11-15 15:38:05 -0300

    feat: add footer

---
+++ frontend/components/Footer/Footer.tsx
@@ -0,0 +1,11 @@
+import S from './Footer.module.scss';
+
+function Footer() {
+  return (
+    <footer className={S.container}>
+      <h4>@antoniohauren</h4>
+    </footer>
+  );
+}
+
+export default Footer;
