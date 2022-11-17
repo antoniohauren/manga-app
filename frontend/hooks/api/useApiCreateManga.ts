a503c73e59ef2bdd12e179e7a448e486a86d870e
antoniohauren <antoniohauren@protonmail.com>
2022-11-17 09:13:41 -0300

    feat: navigate after create new manga

--- frontend/hooks/api/useApiCreateManga.ts
+++ frontend/hooks/api/useApiCreateManga.ts
@@ -1,11 +1,16 @@
 import { CreateMangaDto } from '@domain/manga/dto';
 import axios from 'axios';
+import { useRouter } from 'next/router';
 
 function useApiCreateManga() {
+  const router = useRouter();
+
   async function exec(body: CreateMangaDto) {
     try {
       await axios.post(`http://localhost:3333/manga`, body);
       alert('created!');
+
+      router.push('/');
     } catch (err) {
       console.log(err);
     }
